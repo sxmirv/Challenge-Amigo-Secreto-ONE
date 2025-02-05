38bf1d9c500f3d61a8affe6adf28cf30691c29e1
Samira Gonçalves <s.samirag16@gmail.com>
2025-02-05 14:59:41 -0300

    Adicionar amigos à lista e limpar campo

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//Criando lista para armazenar os nomes
+let nomes = [];
+
+//Função para adicionar amigos à lista
+function addAmigo(){
+    let amigo = document.getElementById('amigo').value.trim(); //trim() para remover espaços extras
+
+    if (amigo === ""){
+        alert("Insira um nome válido!");
+    }
+    else{
+        nomes.push(amigo);
+        console.log(nomes);
+        limpaCampo();
+    }
+}
+
+//Função para limpar o campo quando um nome for adicionado
+function limpaCampo(){
+    amigo = document.getElementById('amigo');
+    amigo.value = "";
+}
+
