b536b3ad3b0991dc93056d9a271dfe2459f9eec8
Samira Gonçalves <s.samirag16@gmail.com>
2025-02-07 09:34:09 -0300

    sortear amigo

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ function addAmigo(){
     let amigo = document.getElementById('amigo').value.trim(); //trim() para remover espaços extras
 
     if (amigo === ""){
-        alert("Insira um nome válido!");
+        alert("Por favor, insira um nome!");
     }
     else{
         nomes.push(amigo);
-        console.log(nomes);
+        //console.log(nomes);
         limpaCampo();
+        atualizaLista();
     }
+
 }
 
 //Função para limpar o campo quando um nome for adicionado
@@ -21,3 +23,34 @@ function limpaCampo(){
     amigo.value = "";
 }
 
+//Função para atualizar a lista de amigos
+function atualizaLista(){
+    const ul = document.getElementById('listaAmigos');
+    ul.innerHTML = ''; //limpando o conteúdo da lista para evitar duplicações
+
+    //Loop para percorrer toda a lista e criar um novo elemento de lista para cada nome
+    for (let item of nomes) {
+        let li = document.createElement('li');
+        li.textContent = item;
+        ul.appendChild(li);
+    }
+}
+
+//Função para sortear o amigo secreto
+function sortearAmigo(){
+
+    if (nomes.length === 0){
+        console.log("Lista vazia!");
+    }
+    else{
+        let indiceSorteado = Math.floor(Math.random() * nomes.length);
+        let nomeSorteado = nomes[indiceSorteado];
+
+        const ul = document.getElementById('listaAmigos');
+        ul.innerHTML = '';
+        
+        let li = document.createElement('li');
+        li.textContent = nomeSorteado;
+        ul.appendChild(li);
+    }
+}
