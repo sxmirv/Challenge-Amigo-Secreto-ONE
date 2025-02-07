717be35e8ade057bc2f100d9640c03f4f4ac9c9b
Samira Gonçalves <s.samirag16@gmail.com>
2025-02-07 10:14:01 -0300

    remover nome sorteado da lista e evitar nomes duplicados

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ function addAmigo(){
     if (amigo === ""){
         alert("Por favor, insira um nome!");
     }
+    else if(nomes.includes(amigo)){
+        alert(`O nome ${amigo} já foi inserido!`); //verifica se um nome já foi inserido
+    }
     else{
-        nomes.push(amigo);
+        nomes.push(amigo); //adiciona o nome à lista
         //console.log(nomes);
         limpaCampo();
         atualizaLista();
@@ -25,13 +28,13 @@ function limpaCampo(){
 
 //Função para atualizar a lista de amigos
 function atualizaLista(){
-    const ul = document.getElementById('listaAmigos');
+    let ul = document.getElementById('listaAmigos'); 
     ul.innerHTML = ''; //limpando o conteúdo da lista para evitar duplicações
 
     //Loop para percorrer toda a lista e criar um novo elemento de lista para cada nome
     for (let item of nomes) {
-        let li = document.createElement('li');
-        li.textContent = item;
+        let li = document.createElement('li'); //cria um elemento de lista
+        li.textContent = item; //adiciona a este elemento o valor de "item"
         ul.appendChild(li);
     }
 }
@@ -41,18 +44,22 @@ function sortearAmigo(){
 
     if (nomes.length === 0){
         let ul = document.getElementById('listaAmigos');
-        ul.innerHTML = "<li>Atenção, lista vazia!</li>";
+        ul.innerHTML = "<li>Atenção, lista vazia! Adicione amigos para sortear.</li>"; //cria um elemento de lista 
         return;
     }
     else{
-        let indiceSorteado = Math.floor(Math.random() * nomes.length);
-        let nomeSorteado = nomes[indiceSorteado];
+        let indiceSorteado = Math.floor(Math.random() * nomes.length); //gera um número inteiro aleatório dentro do intervalo específico (tamanho da lsita de nomes)
+        let nomeSorteado = nomes[indiceSorteado]; //pega o valor do índice sorteado
 
+        //limpa a lista para mostrar o nome sorteado
         let ul = document.getElementById('listaAmigos');
         ul.innerHTML = '';
         
+        //cria um novo elemento de lista para mostrar o nome sorteado
         let li = document.createElement('li');
         li.textContent = nomeSorteado;
         ul.appendChild(li);
+
+        nomes.splice(indiceSorteado, 1); //remove o nome sorteado da lista para que ele não se repita em caso de um novo sorteio
     }
 }
